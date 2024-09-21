f3d174af0c6b6311dc5b8ccb27ef6de01bbc6af2
smnraphael <simonraphael@hotmail.fr>
2024-09-21 12:47:24 +0200

    feat: add real data for user balance

--- components/total-current-balance/total-current-balance.tsx
+++ components/total-current-balance/total-current-balance.tsx
@@ -1,11 +1,30 @@
 'use client';
 
 import { Typography } from '@/components/ui/typography';
+import { useEffect, useState } from 'react';
 
 function TotalCurrentBalance() {
+  const [balance, setBalance] = useState<string | null>(null);
+
+  async function fetchBalance() {
+    const response = await fetch('/api/user/balance');
+    const data = await response.json();
+
+    if (response.ok) {
+      setBalance(data.balance);
+    } else {
+      console.error('Error fetching balance');
+    }
+  }
+
+  useEffect(() => {
+    fetchBalance();
+  }, []);
+
   return (
     <Typography variant='p'>
-      Total current balance: <span className='font-semibold'>14,643.46€</span>
+      Total current balance:{' '}
+      <span className='font-semibold'>{Number(balance).toFixed(2)}€</span>
     </Typography>
   );
 }
